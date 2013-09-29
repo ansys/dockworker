4b487b3f2c2e45562e506f58f2bb2a111d3d8f73
Enos Feedler <jeff.kingyens@gmail.com>
2013-09-29 16:48:31 -0700

    with_bouncy

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
+var bouncy = require('bouncy');
 require('./lib/cleanup');
 require('./lib/launchServices');
 require('./lib/webStreams');
 require('./lib/app');
-require('./lib/server').listen(15000);
+require('./lib/server').listen(15001);
+
+var server = bouncy(function (req, res, bounce) { 
+  bounce(15001);
+});
+
+server.listen(15000);
 
 process.on('SIGTERM', process.exit);
